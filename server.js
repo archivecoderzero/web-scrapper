d8472b7f621129306113f876894480b82a9fd394
unknown <archivecoderzero@gmail.com>
2019-08-01 22:24:00 -0700

    update

--- server.js
+++ server.js
@@ -31,8 +31,9 @@ app.set('view engine', 'handlebars');
 
 
 // Connect to the Mongo DB
-var MONGODBURI = process.env.MONGODBURI || "mongodb://localhost/unit18Populater" ;
- 
+// var MONGODBURI = process.env.MONGODBURI || "mongodb://localhost/unit18Populater" ;
+var MONGODBURI = "mongodb://user1:password1@ds359077.mlab.com:59077/heroku_5ng49cjt" ;
+
 //mongoose.connect(MONGODBURI);
 // connect to mongoDB
 mongoose
@@ -75,7 +76,6 @@ app.get("/scrape1", function(req, res) {
         })
         .catch(function(err) {
           // If an error occurred, log it
-          console.log("scra   article!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1");
           console.log(err);
         });
     });
@@ -89,42 +89,7 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.get("/scrape2", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://www.cnn.com/business/investing").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("h3").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
-
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = "https://www.cnn.com" + $(this)
-        .children("a")
-        .attr("href");
-
-
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
-    });
-
-    // Send a message to the client
-    res.render('index');
-  });
-});
 
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
@@ -136,7 +101,6 @@ app.get("/articles", function(req, res) {
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
-      console.log("article!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1");
       res.json(err);
     });
 });
